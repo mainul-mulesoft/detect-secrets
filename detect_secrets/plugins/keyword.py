60bc25f6a1bd672c622dc534b6cbd4f7219c5475
Mainul Islam <mainul.islam@mulesoft.com>
2022-03-28 19:39:37 +1100

    debugging

--- detect_secrets/plugins/keyword.py
+++ detect_secrets/plugins/keyword.py
@@ -308,7 +308,7 @@ class KeywordDetector(BasePlugin):
                 match = denylist_regex.search(string)
                 if match:
                     has_results = True
-                    print(string)
+                    print(string + " :::: " + group_number)
                     yield match.group(group_number)
 
             if has_results:
