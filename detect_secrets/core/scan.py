33134d8b9d48f5939b509b9be921c207de003c2d
Mainul Islam <mainul.islam@mulesoft.com>
2022-03-29 00:54:35 +1100

    debugging

--- detect_secrets/core/scan.py
+++ detect_secrets/core/scan.py
@@ -377,7 +377,7 @@ def _scan_line(
 
 
 def _is_filtered_out(required_filter_parameters: Iterable[str], **kwargs: Any) -> bool:
-    print("Plugin ##::##" + kwargs.plugin)
+    print("Plugin ##::##" + kwargs)
     for filter_fn in get_filters_with_parameter(*required_filter_parameters):
         try:
             if call_function_with_arguments(filter_fn, **kwargs):
