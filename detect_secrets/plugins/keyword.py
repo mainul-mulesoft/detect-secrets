3fd8ec700f063324f720485f8a893a0743ceea7f
Mainul Islam <mainul.islam@mulesoft.com>
2022-03-28 20:01:02 +1100

    debugging

--- detect_secrets/plugins/keyword.py
+++ detect_secrets/plugins/keyword.py
@@ -322,7 +322,6 @@ class KeywordDetector(BasePlugin):
         **kwargs: Any,
     ) -> Set[PotentialSecret]:
         filetype = determine_file_type(filename)
-        print("File Name::" + str(filename))
         denylist_regex_to_group = REGEX_BY_FILETYPE.get(filetype, QUOTES_REQUIRED_DENYLIST_REGEX_TO_GROUP)  # noqa: E501
         return super().analyze_line(
             filename=filename,
